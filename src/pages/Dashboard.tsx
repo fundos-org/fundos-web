d6bfba8b5d080d595b3fa3b5dc47eee85b82bca0
goutamkota <kgoutamsnpt@gmail.com>
2025-05-04 14:05:07 +0530

    Deal flow page completed

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,6 +6,12 @@ import TransactionDetailsTable from "@/components/custom/TransactionDetailsTable
 function Dashboard() {
   return (
     <>
+      <h2 className="text-4xl">Welcome Back, Kota</h2>
+      <div className="mb-8">
+          <small className="text-gray-500">
+            Measure your advertising ROI and report website traffic
+          </small>
+          </div>
       <StatisticCardList />
       <div className="w-full flex gap-5 mb-5">
         <OverViewChart />
--- src/pages/Deals.tsx
+++ src/pages/Deals.tsx
@@ -1,8 +1,26 @@
+import ShowDeals from "@/components/custom/ShowDeals";
+import StatisticCardList from "@/components/custom/StatisticCardList";
 
 function Deals() {
   return (
-    <div>Deals</div>
-  )
+    <>
+      <header className="flex justify-between items-center">
+        <div>
+          <h2 className="text-4xl">Deals</h2>
+        </div>
+        <button className="bg-white text-black px-4 py-2 rounded-none">
+          Create New Deal
+        </button>
+      </header>
+      <div className="mb-8">
+        <small className="text-gray-500">
+          Track how your deals are performing and manage your portfolio
+        </small>
+      </div>
+      <StatisticCardList />
+      <ShowDeals />
+    </>
+  );
 }
 
-export default Deals
+export default Deals;
